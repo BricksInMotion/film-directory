120fed44eb4e6ec588e4b829a1871a1f7b41c4a6
Caleb Ely <le717@users.noreply.github.com>
2018-11-22 17:06:08 -0500

    Update footer conten

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,5 @@
 <footer>
-  2018 - <?= date('Y'); ?> Bricks in Motion
+  <div>&copy; 2008 &mdash; <?= date('Y'); ?> <a href="http://bricksinmotion.com">Bricks in Motion</a>. All works copyrighted by their respective creators.</div>
+  <div>LEGO is a trademark of the LEGO Group, which does not sponsor, authorize or endorse this web site.</div>
+  <div><a href="http://www.bricksinmotion.com/forums/topic/25304/bricks-in-motion-staff-list/">Site Credits</a> | <a href="http://www.bricksinmotion.com/forums/topic/25303/bricks-in-motion-privacy-policy/">Privacy Policy</a></div>
 </footer>
