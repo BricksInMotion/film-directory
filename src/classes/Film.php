a799c1ac7dbca770deccb54d8f864475b7122d2e
Caleb Ely <le717@users.noreply.github.com>
2018-11-22 17:07:03 -0500

    Update film reviews visuals

--- src/classes/Film.php
+++ src/classes/Film.php
@@ -106,7 +106,7 @@ class Film {
     // This film has no reviews, provide text that says so
     if (count($reviews) === 0) {
       $reviews = new stdClass();
-      $reviews->real_name = 'This film has not been reviewed!';
+      $reviews->real_name = 'This film has not been reviewed';
       $reviews->comments = '';
 
       // We expect an array containing `stdClass`es,
