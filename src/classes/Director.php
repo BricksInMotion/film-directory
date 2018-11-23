4450392eaca651592bcd04ae91ecd261332e8c1f
Caleb Ely <le717@users.noreply.github.com>
2018-11-23 18:19:52 -0500

    Implement Director->get_director_exists method

--- src/classes/Director.php
+++ src/classes/Director.php
@@ -6,4 +6,24 @@ class Director {
   function __construct($id) {
     $this->id = (int) $id;
   }
+
+  /**
+   * Determine if the director exists.
+   *
+   * @return {bool}
+   */
+  function get_director_exists() {
+    // Short circuit the lookup if the ID is zero
+    // Zero is being used to indicate an unknown director
+    if ($this->id === 0) return false;
+
+    require 'src/db-connect.php';
+    $stmt = $pdo->prepare('SELECT
+    1
+    FROM `films_users`
+    WHERE `films_users`.`user_id`= ?
+    LIMIT 1');
+    $stmt->execute([$this->id]);
+    return (bool) $stmt->fetch(PDO::FETCH_OBJ);
+  }
 }
