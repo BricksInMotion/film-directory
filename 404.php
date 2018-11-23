74430f7ed0e3b56287782576066dcc24c0d74804
Caleb Ely <le717@users.noreply.github.com>
2018-11-23 16:25:55 -0500

    Remove reference to non-existant file

--- 404.php
+++ 404.php
@@ -1,13 +1,12 @@
 <?php
 $pageTitle = 'Item Not Found';
-require_once 'src/global.php';
 require_once 'partials/head.php';
 require_once 'partials/header.php';
 ?>
 
-<main>
-  <h2>Page Not Found</h2>
-  <p>Sorry, we can't find that thing you wanted! 😭😭😭</p>
-</main>
+  <main>
+    <h2>Page Not Found</h2>
+    <p>Sorry, we can't find that thing you wanted! 😭😭😭</p>
+  </main>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $pageTitle = 'Home';
-require_once 'src/global.php';
+require_once 'src/db-connect.php';
 require_once 'partials/head.php';
 require_once 'partials/header.php';
 require_once 'src/common-utils.php';
