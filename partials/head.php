e2ebbb4de816f53d91569bdae601cbcc204a0d42
Caleb Ely <le717@users.noreply.github.com>
2018-11-23 11:01:15 -0500

    Site title adjustment, remove unused file

--- partials/head.php
+++ partials/head.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?= $pageTitle; ?> | Bricks in Motion Film Directory</title>
+  <title><?= $pageTitle; ?> | Bricks in Motion Film Directory Archive</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,700|Zilla+Slab:300,500">
   <link rel="stylesheet" href="css/style.css">
 </head>
