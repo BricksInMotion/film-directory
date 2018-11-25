d0619df0c28fb70c7d764417f5ad59e8511dc2c1
Caleb Ely <le717@users.noreply.github.com>
2018-11-25 12:59:42 -0500

    Add crude nav

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,9 @@
 <header>
   <img class="site-logo" alt="Bricks in Motion logo" width="150" src="site-images/logo.png">
   <h1>Film Directory Archive</h1>
+
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="search.php">Search</a>
+  </nav>
 </header>
