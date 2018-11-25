97dc23540034ce0a897334ad92a497d108756b5f
Caleb Ely <le717@users.noreply.github.com>
2018-11-25 11:18:04 -0500

    Add new logo

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,4 @@
 <header>
-  <img alt="Bricks in Motion logo" src="site-images/logo_bim.png">
+  <img style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.2)); margin-right: 0.5em;" alt="Bricks in Motion logo" width="150" src="site-images/logo.png">
   <h1>Film Directory Archive</h1>
 </header>
