38ca74a73ea51445f2bcff7afa185dab73971e1d
Caleb Ely <le717@users.noreply.github.com>
2018-11-25 11:42:41 -0500

    Completely flip the color scheme

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,4 @@
 <header>
-  <img style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.2)); margin-right: 0.5em;" alt="Bricks in Motion logo" width="150" src="site-images/logo.png">
+  <img class="site-logo" alt="Bricks in Motion logo" width="150" src="site-images/logo.png">
   <h1>Film Directory Archive</h1>
 </header>
