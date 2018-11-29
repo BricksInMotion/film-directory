6cc5e4eba99f8f847cc2a4d1762a410451017280
Caleb Ely <le717@users.noreply.github.com>
2018-11-28 22:49:22 -0500

    Add an index to films.incrementor

--- sql/make-film-incrementor.sql
+++ sql/make-film-incrementor.sql
@@ -1,7 +1,10 @@
 -- Add the new column
--- TODO: add index
 ALTER TABLE `films`
 ADD COLUMN `incrementor` INT(12) UNSIGNED NOT NULL DEFAULT '0' AFTER `edit`;
+
+-- Add an index to improve lookup
+ALTER TABLE `films`
+ADD INDEX `incrementor` (`incrementor`);
 
 -- Create our incrementor
 SET @i = 0;
@@ -9,4 +12,4 @@ SET @i = 0;
 -- Add our incrementor to the data
 UPDATE `films`
 SET `incrementor` = (@i := @i + 1)
-ORDER BY `films`.`date_create` ASC';
+ORDER BY `films`.`date_create` ASC;
