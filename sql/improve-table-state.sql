1abb68ff5c9b74e97ce464c6e95f6a3be3b0a07b
Caleb Ely <le717@users.noreply.github.com>
2018-11-30 00:52:28 -0500

    Table cleanup actions

--- sql/improve-table-state.sql
+++ sql/improve-table-state.sql
@@ -2,6 +2,29 @@
 ALTER TABLE `films`
 ADD INDEX `title` (`title`);
 
--- TODO: Purge unused columns
+-- Rename `genres` table to be consistent with other table names
+RENAME TABLE `genres` TO `films_all_genres`;
 
--- TODO: Rename `genre` table to `film_genre` to be consistent
+-- Remove unused tables
+DROP TABLE `films_tags`;
+DROP TABLE `films_ratings`;
+
+-- Remove unused and/or useless columns
+ALTER TABLE `films`
+  DROP COLUMN `img_poster`,
+  DROP COLUMN `img_frame`,
+  DROP COLUMN `time_reviewed`,
+  DROP COLUMN `rate_over`,
+  DROP COLUMN `rate_story`,
+  DROP COLUMN `rate_ani`,
+  DROP COLUMN `rate_cine`,
+  DROP COLUMN `rate_fx`,
+  DROP COLUMN `rate_sound`,
+  DROP COLUMN `rate_music`,
+  DROP COLUMN `warn_desc`,
+  DROP COLUMN `date_edit`,
+  DROP COLUMN `edit`;
+
+-- Remove the primary genre indicator (it is not respected)
+-- ALTER TABLE `films_genre`
+--   DROP COLUMN `is_primary`;
