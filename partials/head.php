487af41ad1ec9acea50005724cac85688a07b3c1
Caleb Ely <le717@users.noreply.github.com>
2018-12-02 13:28:57 -0500

    Add favicon

--- partials/head.php
+++ partials/head.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?= $pageTitle; ?> | Bricks in Motion Film Directory Archive</title>
+  <meta name="theme-color" content="#10141f">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:500,700|Montserrat:400,400i,700">
   <link rel="stylesheet" href="css/style.css">
   <?php
