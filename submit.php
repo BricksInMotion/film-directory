d5cb7d9cf1bd6b8d005ee50d15e1088c80875646
Caleb Ely <le717@users.noreply.github.com>
2018-12-01 22:10:18 -0500

    Add stub submission page

---
+++ submit.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = 'Submit';
+$pageStyles = ['style-submit.css'];
+require_once 'partials/head.php';
+require_once 'partials/header.php';
+?>
+
+<main>
+  <?php require 'partials/navi.html'; ?>
+
+  <section>
+  <h2>Submit a film</h2>
+  <p></p>
+  </section>
+</main>
+
+<?php require 'partials/footer.php'; ?>
+</body>
+</html>
