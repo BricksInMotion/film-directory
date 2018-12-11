465f3a534b2452939c3e154eacb6b5f5feb34976
Caleb Ely <le717@users.noreply.github.com>
2018-12-10 19:16:35 -0500

    Refactor input styling

--- search.php
+++ search.php
@@ -12,7 +12,7 @@
     <h2>Search by film title</h2>
     <p>Is there a film you <em>think</em> has a certain title but don't know for sure? Try searching and see if it comes up!</p>
 
-    <label>
+    <label class="search-input-label">
       <input type="text" class="search-input" autocomplete="on" placeholder="The Citizen of the Year">
     </label>
   </section>
