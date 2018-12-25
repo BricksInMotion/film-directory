2807ffd0f679dedc2167b17ec5cac9095f52abb3
Caleb Ely <le717@users.noreply.github.com>
2018-12-25 15:43:36 -0500

    URL path fixes

--- partials/head.php
+++ partials/head.php
@@ -1,3 +1,7 @@
+<?php
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +10,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?= $pageTitle; ?> | Bricks in Motion Film Directory Archive</title>
   <meta name="theme-color" content="#10141f">
-  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:500,700|Montserrat:400,400i,700">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="/css/style.css">
   <?php
     if (isset($pageStyles)):
       foreach ($pageStyles as $css):
-        echo "<link rel='stylesheet' href='css/{$css}'>\n";
+        echo "<link rel='stylesheet' href='/css/{$css}'>\n";
       endforeach;
     endif;
     ?>
--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <header>
-  <img class="site-logo" alt="Bricks in Motion logo" width="150" src="site-images/logo.png">
+  <img class="site-logo" alt="Bricks in Motion logo" width="150" src="/site-images/logo.png">
   <h1 class="bim-title">Bricks in<br><span>Motion</span></h1>
   <h1 class="archive-title">Film Directory Archive</h1>
 </header>
