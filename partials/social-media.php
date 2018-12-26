408ef2ab3f7e47329e40506f01f09c39c3f24359
Caleb Ely <le717@users.noreply.github.com>
2018-12-25 22:56:01 -0500

    films -> brickfilms

--- partials/social-media.php
+++ partials/social-media.php
@@ -5,11 +5,11 @@
 <meta property="og:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
 <meta property="og:url" content="http://films.galidor.club">
 <meta property="og:image" content="http://films.galidor.club/site-images/logo.webp">
-<meta property="og:description" content="Discover classic films on the Bricks in Motion Film Directory Archive!">
+<meta property="og:description" content="Discover classic brickfilms on the Bricks in Motion Film Directory Archive!">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@BricksInMotion">
 <meta name="twitter:creator" content="@BricksInMotion">
 <meta name="twitter:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
-<meta name="twitter:description" content="Discover classic films on the Bricks in Motion Film Directory Archive!">
+<meta name="twitter:description" content="Discover classic brickfilms on the Bricks in Motion Film Directory Archive!">
 <meta name="twitter:image" content="http://films.galidor.club/site-images/logo.webp">
 <meta name="twitter:image:alt" content="http://films.galidor.club/site-images/logo.webp">
