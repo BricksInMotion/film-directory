0a053fbeb5756e182deb22137621cee958776f90
Caleb Ely <le717@users.noreply.github.com>
2018-12-25 23:32:39 -0500

    Better form reset/page load handling

--- js/submit.js
+++ js/submit.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var qForm = document.querySelector("form");
 var qContentWarningVio = document.querySelector("#film-vio-rate");
 var qContentWarningLang = document.querySelector("#film-lang-rate");
 var qContentWarningSex = document.querySelector("#film-sex-rate");
@@ -22,16 +23,46 @@ function handleSliderChange(e) {
   warningLabels[e.target.id].textContent = warningValues[Number.parseInt(e.target.value)];
 }
 
-// Reset the sliders on page load
-qContentWarningVio.value = "0";
-qContentWarningLang.value = "0";
-qContentWarningSex.value = "0";
+/**
+ * Reset the content warning sliders.
+ */
+function resetSliderValues() {
+  qContentWarningVio.value = "0";
+  qContentWarningLang.value = "0";
+  qContentWarningSex.value = "0";
+
+  // Trigger the change event to update the labels
+  var event = new Event("change");
+  qContentWarningVio.dispatchEvent(event);
+  qContentWarningLang.dispatchEvent(event);
+  qContentWarningSex.dispatchEvent(event);
+}
+
+/**
+ * Default the release date to today.
+ */
+function defaultReleseDate() {
+  var curDate = new Date();
+  qBasicInfoReleaseDate.value = `${curDate.getFullYear()}-${curDate.getMonth() + 1}-${curDate.getDate()}`;
+}
 
 // Handle the sliders being updated
 qContentWarningVio.addEventListener("change", handleSliderChange);
 qContentWarningLang.addEventListener("change", handleSliderChange);
 qContentWarningSex.addEventListener("change", handleSliderChange);
 
-// Default the release date to today
-var curDate = new Date();
-qBasicInfoReleaseDate.value = `${curDate.getFullYear()}-${curDate.getMonth() + 1}-${curDate.getDate()}`;
+
+// Reset the form's custom elements
+qForm.addEventListener("reset", function() {
+  // https://stackoverflow.com/a/21641295
+  setTimeout(function() {
+    defaultReleseDate();
+    resetSliderValues();
+  });
+});
+
+// Set default form values on page load
+document.addEventListener("DOMContentLoaded", function() {
+  defaultReleseDate();
+  resetSliderValues();
+}, {once: true});
