5542dd50d8a1f4f12dc5521496c16b0329514db6
Caleb Ely <le717@users.noreply.github.com>
2018-12-25 22:52:31 -0500

    Use large twitter card

--- partials/social-media.php
+++ partials/social-media.php
@@ -1,12 +1,12 @@
 <meta name="title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
-<meta name="description" content="Discover classic films on the Bricks in Motion Film Directory Archive!">
+<meta name="description" content="Discover classic brickfilms on the Bricks in Motion Film Directory Archive!">
 <meta property="og:site_name" content="BricksinMotion.com">
 <meta property="og:type" content="website">
 <meta property="og:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
 <meta property="og:url" content="http://films.galidor.club">
 <meta property="og:image" content="http://films.galidor.club/site-images/logo.webp">
 <meta property="og:description" content="Discover classic films on the Bricks in Motion Film Directory Archive!">
-<meta name="twitter:card" content="summary">
+<meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@BricksInMotion">
 <meta name="twitter:creator" content="@BricksInMotion">
 <meta name="twitter:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
