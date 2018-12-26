cde67f70d148aa9831467533812b90822110ed5d
Caleb Ely <le717@users.noreply.github.com>
2018-12-25 21:42:47 -0500

    Add webp version of logo

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,8 @@
 <header>
-  <img class="site-logo" alt="Bricks in Motion logo" width="150" src="/site-images/logo.png">
+  <picture class="site-logo">
+    <source type="image/webp" srcset="/site-images/logo.webp">
+    <img alt="Bricks in Motion logo" width="150" height="150" src="/site-images/logo.png">
+  </picture>
   <h1 class="bim-title">Bricks in<br><span>Motion</span></h1>
   <h1 class="archive-title">Film Directory Archive</h1>
 </header>
