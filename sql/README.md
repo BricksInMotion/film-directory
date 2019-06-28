5c6ff827b1179bb26ef71bf0219181fdf350a3aa
rioforce <rioforce@live.com>
2019-06-27 21:11:08 -0400

    Add SQL script to make table used in last commit

--- sql/README.md
+++ sql/README.md
@@ -2,6 +2,7 @@ These scripts alter the database to be compatible with newer MySQL/MariaDB versi
 
 1. `make-films-users.sql`
 1. `make-films-topics.sql`
+1. `make-films-advisories.sql`
 1. `make-film-incrementor.sql`
 1. `make-film-reborn-status.sql`
 1. `improve-database-state.sql`
---
+++ sql/make-films-advisories.sql
@@ -0,0 +1,29 @@
+-- Create a new table to store film advisories
+CREATE TABLE `films_advisories` (
+  `film_id` INT(12) UNSIGNED NOT NULL,
+  `sex` ENUM('0','1','2','3') NOT NULL,
+  `language` ENUM('0','1','2','3') NOT NULL,
+  `violence` ENUM('0','1','2','3') NOT NULL,
+  PRIMARY KEY (`film_id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB;
+
+-- Remove all null values from the existing data
+UPDATE `films`
+SET `warn_sex` = '0'
+WHERE `warn_sex` IS NULL;
+
+UPDATE `films`
+SET `warn_lang` = '0'
+WHERE `warn_lang` IS NULL;
+
+UPDATE `films`
+SET `warn_vio` = '0'
+WHERE `warn_vio` IS NULL;
+
+-- Copy the ratings into the new table
+INSERT INTO `films_advisories`(`film_id`, `language`, `sex`, `violence`)
+SELECT `id`, `warn_lang`, `warn_sex`, `warn_vio`
+FROM `films`
+ORDER BY `films`.`id`;
