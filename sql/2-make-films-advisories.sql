c35916bd1b14884d1796e38673d7d6abb2f406c4
rioforce <rioforce@users.noreply.github.com>
2020-01-11 15:20:28 -0500

    Continue running script even if errors arise

--- sql/2-make-films-advisories.sql
+++ sql/2-make-films-advisories.sql
@@ -10,20 +10,20 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB;
 
 -- Remove all null values from the existing data
-UPDATE `films`
+UPDATE IGNORE `films`
 SET `warn_sex` = '0'
 WHERE `warn_sex` IS NULL;
 
-UPDATE `films`
+UPDATE IGNORE `films`
 SET `warn_lang` = '0'
 WHERE `warn_lang` IS NULL;
 
-UPDATE `films`
+UPDATE IGNORE `films`
 SET `warn_vio` = '0'
 WHERE `warn_vio` IS NULL;
 
 -- Copy the ratings into the new table
-INSERT INTO `films_advisories`(`film_id`, `language`, `sex`, `violence`)
+INSERT IGNORE INTO `films_advisories`(`film_id`, `language`, `sex`, `violence`)
 SELECT `id`, `warn_lang`, `warn_sex`, `warn_vio`
 FROM `films`
 ORDER BY `films`.`id`;
