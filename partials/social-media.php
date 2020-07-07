64e6114b4b1b84687e395adbddb5ee3f4bcccd52
Caleb Ely <le717@users.noreply.github.com>
2020-07-07 18:25:12 -0400

    fix domain

--- partials/social-media.php
+++ partials/social-media.php
@@ -3,13 +3,13 @@
 <meta property="og:site_name" content="BricksinMotion.com">
 <meta property="og:type" content="website">
 <meta property="og:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
-<meta property="og:url" content="http://films.galidor.club">
-<meta property="og:image" content="http://films.galidor.club/site-images/logo.webp">
+<meta property="og:url" content="https://films.bricksinmotion.com">
+<meta property="og:image" content="https://www.bricksinmotion.com/images/site/BiM-Logo-Site.png">
 <meta property="og:description" content="Discover classic brickfilms on the Bricks in Motion Film Directory Archive!">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@BricksInMotion">
 <meta name="twitter:creator" content="@BricksInMotion">
 <meta name="twitter:title" content="<?= $pageTitle; ?> | Bricks in Motion Film Directory Archive">
 <meta name="twitter:description" content="Discover classic brickfilms on the Bricks in Motion Film Directory Archive!">
-<meta name="twitter:image" content="http://films.galidor.club/site-images/logo.webp">
-<meta name="twitter:image:alt" content="http://films.galidor.club/site-images/logo.webp">
+<meta name="twitter:image" content="https://www.bricksinmotion.com/images/site/BiM-Logo-Site.png">
+<meta name="twitter:image:alt" content="https://www.bricksinmotion.com/images/site/BiM-Logo-Site.png">
